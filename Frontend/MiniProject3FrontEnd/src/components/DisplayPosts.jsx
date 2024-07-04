42ccd1268c222adc4b69e7ef782d2c8cccf4abf9
nelg62 <glen.harding.nz@gmail.com>
2024-07-04 13:47:36 +1200

    creating posts and comments working will try working on deleting soon

--- Frontend/MiniProject3FrontEnd/src/components/DisplayPosts.jsx
+++ Frontend/MiniProject3FrontEnd/src/components/DisplayPosts.jsx
@@ -1,4 +1,3 @@
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
 import { usePostContext } from "../context/PostContext";
 import PostList from "./PostList";
 
@@ -9,7 +8,7 @@ export default function DisplayPosts() {
   return (
     <>
       {postList.map((post) => {
-        return <PostList post={post}></PostList>;
+        return <PostList key={post.id} post={post}></PostList>;
       })}
     </>
   );
