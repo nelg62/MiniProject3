2decc1a39c747193c080bb2aeb09110e1af94445
amitabhdas96 <amitabh.sweden@gmail.com>
2024-07-06 08:29:40 +1200

    ok

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
+let dbConnect = require("./dbConnect");
+
 const postRoutes = require("./routes/postRoutes");
 const likeRoutes = require("./routes/likeRoutes");
 const userRoutes = require("./routes/userRoutes");
