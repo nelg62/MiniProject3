0d232378a25430cd1be47162b03abd3c419ff3a9
nelg62 <glen.harding.nz@gmail.com>
2024-08-09 15:10:01 +1200

    change database names

--- Backend/src/dbConnect.js
+++ Backend/src/dbConnect.js
@@ -5,9 +5,9 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.USER_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.USER_HOST,
     dialect: "mysql",
     dialectOptions: {
       connectTimeout: 60000,
