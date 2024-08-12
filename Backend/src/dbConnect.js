a433bd2038aa29a5fa0e4b542fdfcd68354e233e
nelg62 <glen.harding.nz@gmail.com>
2024-08-13 11:07:22 +1200

    change database file to connect to database

--- Backend/src/dbConnect.js
+++ Backend/src/dbConnect.js
@@ -1,30 +1,54 @@
 "use strict";
+
 const { Sequelize } = require("sequelize");
 
 // Sequelize to database using environment variable credentials
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.USER_PASSWORD,
-  {
-    host: process.env.USER_HOST,
-    dialect: "mysql",
-    dialectOptions: {
-      connectTimeout: 60000,
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    logging: console.log,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 60000,
-      idle: 10000,
-    },
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.USER_PASSWORD,
+//   {
+//     host: process.env.USER_HOST,
+//     dialect: "mysql",
+//     dialectOptions: {
+//       connectTimeout: 60000,
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//     },
+//     logging: console.log,
+//     pool: {
+//       max: 10,
+//       min: 0,
+//       acquire: 60000,
+//       idle: 10000,
+//     },
+//   }
+// );
+
+const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.USER_PASSWORD,
+
+  host: process.env.USER_HOST,
+  port: process.env.USER_PORT,
+  dialect: "mysql",
+
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+
+  logging: console.log,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 60000,
+    idle: 10000,
+  },
+});
 
 // Function to connect to MySQL database
 const connectMysql = async () => {
