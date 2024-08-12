088b7acb1c01d23acd66590e367eb774de4ce704
nelg62 <glen.harding.nz@gmail.com>
2024-08-12 18:27:33 +1200

    cors post form

--- Frontend/MiniProject3FrontEnd/src/components/PostForm.jsx
+++ Frontend/MiniProject3FrontEnd/src/components/PostForm.jsx
@@ -30,6 +30,7 @@ export default function PostForm({ existingPost, onPostSaved }) {
         method: method,
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(post),
+        mode: "cors",
       });
 
       const result = await response.json();
